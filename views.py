a7517c2a0efc14e2cd425359ae984784d6973b55
Mỹ An <81601941+buingocmya@users.noreply.github.com>
2023-01-03 01:48:03 +0700

    Update views.py

--- views.py
+++ views.py
@@ -9,31 +9,23 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from scipy import spatial
 import requests
+import math
 from googletrans import Translator
 
-def test(request):
-    #next = request.GET('next','')
-    eng =  request.POST.get('engsenten')
-    viet =  request.POST.get('vietsenten')
-    submit = request.POST.get('submit')
 
-    viet=trans(viet)
-
-    englist = preprocessing(eng)
-    vietlist= preprocessing(viet)
-    
-    result = simiS(englist,vietlist)
-
-    context= {'engsenten': preprocessing(eng), 'vietsenten': preprocessing(viet), 'submit': submit, 'result': result}
-    return render(request, 'home.html', context )
-
-def get_home(request):
-    return render(request,'home.html')
-
-def trans(text):
-    translate = Translator()
-    result = translate.translate(text)
-    return result.text
+def wup_simi(text1, text2):
+    if (text1 == text2):
+        return 1.0;
+    w1 = wn.synsets(text1)
+    w2 = wn.synsets(text2)
+    maxScore = 0
+    for synset1 in w1:
+        for synset2 in w2:
+            currScore = wn.wup_similarity(synset1, synset2)
+            if currScore > maxScore:
+                maxScore = currScore
+    #print(text1 + ' ' + text2+ ' ', maxScore)
+    return maxScore
 
 def simiS(text1, text2):
     listtext = text1 + text2
@@ -61,21 +53,67 @@ def simiS(text1, text2):
     print (result)
     return result
 
-def wup_simi(text1, text2):
-    #print(text1 + ' ' + text2)
-    if (text1 == text2):
-        return 1.0;
-    w1 = wn.synsets(text1)
-    #w1.lowest_common_hypernyms(w1, use_min_depth=True)
-    w2 = wn.synsets(text2)
-    maxScore = 0
-    for synset1 in w1:
-        for synset2 in w2:
-            currScore = wn.wup_similarity(synset1, synset2)
-            if currScore > maxScore:
-                maxScore = currScore
-    #print(text1 + ' ' + text2+ ' ', maxScore)
-    return maxScore
+def simiR(text1, text2):
+    pi = 0.8
+    listtext = text1 + text2
+    vector1 = []
+    for i in range(len(listtext)):
+        max = 0
+        index = 0
+        for w1 in text1:
+            tmp = wup_simi(listtext[i],w1)
+            if (tmp>max and tmp>pi):
+                max = tmp
+                index = i 
+        vector1.append(index);
+    print('vector1 =', vector1)
+    
+    vector2 = []
+    for i in range(len(listtext)):
+        max = 0
+        index = 0
+        for w2 in text2:
+            tmp = wup_simi(listtext[i],w2)
+            if (tmp>max and tmp>pi):
+                max = tmp 
+                index = i
+        vector2.append(index);
+    print('vector2 =', vector2)
+    a=0
+    b=0
+    for i in range (0,len(listtext)):
+        a=a+(vector1[i]-vector2[i])*(vector1[i]-vector2[i])
+        b=b+(vector1[i]+vector2[i])*(vector1[i]+vector2[i])
+    result= 1 - math.sqrt(a)/math.sqrt(b)
+    print (result)
+    return result
+
+
+def test(request):
+    #next = request.GET('next','')
+    eng =  request.POST.get('engsenten')
+    viet =  request.POST.get('vietsenten')
+    submit = request.POST.get('submit')
+
+    viet=trans(viet)
+
+    englist = preprocessing(eng)
+    vietlist= preprocessing(viet)
+    
+    result = 0.5 * simiS(englist,vietlist) + 0.5 * simiR(englist,vietlist)
+
+    context= {'engsenten': preprocessing(eng), 'vietsenten': preprocessing(viet), 'submit': submit, 'result': result}
+    print ("result: ",result)
+    return render(request, 'home.html', context )
+
+def get_home(request):
+    return render(request,'home.html')
+
+def trans(text):
+    translate = Translator()
+    result = translate.translate(text)
+    return result.text
+
 
 def preprocessing(text):
     #bỏ in hoa
@@ -120,14 +158,6 @@ def preprocessing(text):
     #return "".join(lem_sentence)
 
 """
-def simS(t1,t2):
-    return 0
-
-def simR(t1,t2):
-    return 0
-
-def show_result(reponse):
-    return 1
 
 
 #1. cd mysite
